e7743575f09da9fbc886567d321c72d19c32e9e1
Douw Steyl <douwjnr@gmail.com>
2021-09-10 11:18:04 +0200

    Working SQL
    Check register class to see how the SQL query works, and make amendments to be able

---
+++ src/lms/Librarymanagementsystem.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lms;
+
+import javax.swing.JFrame;
+import lms.resources.adminController.AdminOperations;
+import lms.resources.view.AdminMenu;
+import lms.resources.view.TestFrontend;
+
+/**
+ *
+ * @author Keanu
+ */
+public class Librarymanagementsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        JFrame frame = new TestFrontend();
+//        frame.setVisible(true);
+       
+
+//        AdminOperations adminOperations = new AdminOperations();
+//        adminOperations.login();
+    }
+}
---
+++ src/lms/resources/repository/DatabaseConnection.java
@@ -0,0 +1,44 @@
+package lms.resources.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Keanu Johnston
+ */
+public class DatabaseConnection {
+    
+    public static Connection getCon()
+    {
+    
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lmsdatabase?zeroDateTimeBehavior=convertToNull","root","Douw1998");
+            return con;
+            
+        }catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+//    public Connection databaseLink;
+//
+//    public Connection getConnection(){
+//        String databaseName = "jdbc-library-system-app";
+//        String databaseUser = "root";
+//        String databasePassword = "99KJ@PROm";
+//        String url = "jdbc:mysql://localhost:3306/" + databaseName;
+//
+//        try{
+//            Class.forName("com.mysql.jdbc.Driver");
+//            databaseLink = DriverManager.getConnection(url,databaseUser,databasePassword);
+//        }catch(Exception e) {
+//            e.printStackTrace();
+//            e.getCause();
+//        }
+//
+//        return databaseLink;
+//    }
+}
