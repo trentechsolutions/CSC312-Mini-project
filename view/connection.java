846ec99586c9b5f21744cc97eaa69e4c127570b4
tashinga123 <43650510+tashinga123@users.noreply.github.com>
2021-09-23 15:42:36 +0200

    Add files via upload

---
+++ view/connection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lms.resources.view;
+
+import java.sql.Statement;
+
+/**
+ *
+ * @author Hamilton
+ */
+class connection {
+
+    static Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
